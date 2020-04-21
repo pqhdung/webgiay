6a4e3de1c99e4ecc071844249dace6047f356f64
pqhdung <tnydung@gmail.com>
2020-04-21 12:26:56 +0700

    first commit

---
+++ resources/views/thanhvien.blade.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Thành viên nhóm 10</title>
+		<style>
+			.thanhvien{
+				font-size: 15px;
+				color: #636e72;
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<h1 style="text-align: center;color: #F44336">THÀNH VIÊN NHÓM 10</h1>
+		<hr/>
+		<div class="thanhvien">DH51600594 - Phùng Quang Hoàng Dũng - D16-TH02</div>
+		<div class="thanhvien">DH51600961 - Nguyễn Công Minh - D16-TH02</div>
+		<div class="thanhvien">DH51600935	- Lâm Tăng Bảo Long - D16-TH02</div>
+		<div class="thanhvien">DH51600604	- Lê Văn Huỳnh Mai - D16_TH02</div>
+	</body>
+</html>
