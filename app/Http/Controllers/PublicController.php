df88b0ea4edbca680629050df4f19040dc235225
pqhdung <tnydung@gmail.com>
2020-05-01 01:05:10 +0700

    loading index

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -15,8 +15,10 @@ class PublicController extends Controller
     public function index()
     {
         $producer = Producer::select('id','name_producer')->get();
-        $shoes    = Shoes::select('id','name','product','price','images')->get();
-        return view('PublicPage.SubPage.contents',['producer'=>$producer,'shoes'=>$shoes]);
+        $shoes    = Shoes::select('id','name','product','price','images')->orderBy('id','desc')->offset(0)->take(4)->get();
+        $shoes2    = Shoes::select('id','name','product','price','images')->orderBy('id','desc')->offset(4)->take(2)->get();
+        $shoes3    = Shoes::select('id','name','product','price','images')->orderBy('id','desc')->offset(6)->take(6)->get();
+        return view('PublicPage.SubPage.contents',['producer'=>$producer,'shoes'=>$shoes,'shoes2'=>$shoes2,'shoes3'=>$shoes3]);
     }
 
 }
--- resources/views/PublicPage/SubPage/contents.blade.php
+++ resources/views/PublicPage/SubPage/contents.blade.php
@@ -67,31 +67,21 @@
       </div><!-- /.collection-top -->
 
       <div class="collection-middle">
+        @foreach ($shoes2 as $Shoes2)
         <div class="col-sm-6">
           <div class="item text-center">
-            <h2 class="item-no">05.</h2><!-- /.item-no -->
-            <h3 class="item-title">Blue sharkskin</h3><!-- /.item-title -->
+            <h2 class="item-no">{{$num}}</h2><!-- /.item-no -->
+            <h3 class="item-title">{{$Shoes2->name}}</h3><!-- /.item-title -->
             <p class="description">
-              That afternoon I had been worrying my brokers about investments to give my mind something to work on, and on my way home
+              {{$Shoes2->product}}
             </p><!-- /.description -->
-            <div class="item-thumbnail"><img class="img-responsive" src="fontend/images/home06/5.png" alt="Item Thumbnail"></div><!-- /.item-thumbnail -->
-            <div class="item-price"><span class="currency">$</span><span class="price">454.00</span></div><!-- /.item-price -->
+            <div class="item-thumbnail"><img class="img-responsive" src="upload/{{$Shoes2->images}}" alt="Item Thumbnail"></div><!-- /.item-thumbnail -->
+            <div class="item-price"><span class="price">{{number_format($Shoes2->price,0, ",",".")}}</span><span class="currency">vnd</span></div><!-- /.item-price -->
             <a href="#" class="btn">Start shopping now</a><!-- /.btn -->
           </div><!-- /.item -->
         </div>
-
-        <div class="col-sm-6">
-          <div class="item text-center">
-            <h2 class="item-no">06.</h2><!-- /.item-no -->
-            <h3 class="item-title">Blue sharkskin</h3><!-- /.item-title -->
-            <p class="description">
-              That afternoon I had been worrying my brokers about investments to give my mind something to work on, and on my way home
-            </p><!-- /.description -->
-            <div class="item-thumbnail"><img class="img-responsive" src="fontend/images/home06/6.png" alt="Item Thumbnail"></div><!-- /.item-thumbnail -->
-            <div class="item-price"><span class="currency">$</span><span class="price">454.00</span></div><!-- /.item-price -->
-            <a href="#" class="btn">Start shopping now</a><!-- /.btn -->
-          </div><!-- /.item -->
-        </div>
+        <?php $num++; ?>
+        @endforeach
 
         <div class="col-sm-12">
           <div class="item text-center">
@@ -112,45 +102,21 @@
       </div><!-- /.collection-middle -->
 
       <div class="collection-bottom">
-        <div class="col-sm-4">
-          <div class="item text-center">
-            <h2 class="item-no">08.</h2><!-- /.item-no -->
-            <h3 class="item-title">Classic shoes</h3><!-- /.item-title -->
-            <p class="description">
-              That afternoon I had been worrying my brokers about investments to give my mind something to work on, and on my way home
-            </p><!-- /.description -->
-            <div class="item-thumbnail"><img class="img-responsive" src="fontend/images/home06/10.png" alt="Item Thumbnail"></div><!-- /.item-thumbnail -->
-            <div class="item-price"><span class="currency">$</span><span class="price">454.00</span></div><!-- /.item-price -->
-            <a href="#" class="btn">Start shopping now</a><!-- /.btn -->
-          </div><!-- /.item -->
-        </div>
-
-        <div class="col-sm-4">
-          <div class="item text-center">
-            <h2 class="item-no">09.</h2><!-- /.item-no -->
-            <h3 class="item-title">Messenger bags</h3><!-- /.item-title -->
-            <p class="description">
-              That afternoon I had been worrying my brokers about investments to give my mind something to work on, and on my way home
-            </p><!-- /.description -->
-            <div class="item-thumbnail"><img class="img-responsive" src="fontend/images/home06/11.png" alt="Item Thumbnail"></div><!-- /.item-thumbnail -->
-            <div class="item-price"><span class="currency">$</span><span class="price">454.00</span></div><!-- /.item-price -->
-            <a href="#" class="btn">Start shopping now</a><!-- /.btn -->
-          </div><!-- /.item -->
-        </div>
-
-        <div class="col-sm-4">
-          <div class="item text-center">
-            <h2 class="item-no">10.</h2><!-- /.item-no -->
-            <h3 class="item-title">Leather belts</h3><!-- /.item-title -->
-            <p class="description">
-              That afternoon I had been worrying my brokers about investments to give my mind something to work on, and on my way home
-            </p><!-- /.description -->
-            <div class="item-thumbnail"><img class="img-responsive" src="fontend/images/home06/12.png" alt="Item Thumbnail"></div><!-- /.item-thumbnail -->
-            <div class="item-price"><span class="currency">$</span><span class="price">454.00</span></div><!-- /.item-price -->
-            <a href="#" class="btn">Start shopping now</a><!-- /.btn -->
-          </div><!-- /.item -->
-        </div>
-
+       @foreach ($shoes3 as $Shoes3)
+       <div class="col-sm-4">
+        <div class="item text-center">
+          <h2 class="item-no">{{$num}}</h2><!-- /.item-no -->
+          <h3 class="item-title">{{$Shoes3->name}}</h3><!-- /.item-title -->
+          <p class="description">
+            {{$Shoes3->product}}
+          </p><!-- /.description -->
+          <div class="item-thumbnail"><img class="img-responsive" src="upload/{{$Shoes3->images}}" alt="Item Thumbnail"></div><!-- /.item-thumbnail -->
+          <div class="item-price"><span class="price">{{number_format($Shoes3->price,0, ",",".")}}</span><span class="currency">vnd</span></div><!-- /.item-price -->
+          <a href="#" class="btn">Start shopping now</a><!-- /.btn -->
+        </div><!-- /.item -->
+      </div>
+      <?php $num++; ?>
+       @endforeach
       </div><!-- /.collection-bottom -->
     </section><!-- /.collection -->
 
