2c79c559459ae8fa03cf064065738ace155fe299
pqhdung <tnydung@gmail.com>
2020-05-24 12:10:48 +0700

    the last commit hoang dung

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -9,16 +9,24 @@ use Illuminate\Support\Facades\DB;
 use App\Producer;
 use App\Category;
 use App\Shoes;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 
 class PublicController extends Controller
 {
     public function index()
     {
+
+        // $client = new Client();
+        // $request = $client->request('GET','http://api.openweathermap.org/data/2.5/weather?id=1566083&appid=85891f0163a9af053561b98a60f6713b');
+        $request = Http::get('http://api.openweathermap.org/data/2.5/weather?id=1566083&appid=85891f0163a9af053561b98a60f6713b');
+        // dd($request->json());
+
         $producer = Producer::select('id','name_producer')->get();
         $shoes    = Shoes::select('id','name','product','price','images')->orderBy('id','desc')->offset(0)->take(4)->get();
         $shoes2    = Shoes::select('id','name','product','price','images')->orderBy('id','desc')->offset(4)->take(2)->get();
         $shoes3    = Shoes::select('id','name','product','price','images')->orderBy('id','desc')->offset(6)->take(3)->get();
-        return view('PublicPage.SubPage.contents',['producer'=>$producer,'shoes'=>$shoes,'shoes2'=>$shoes2,'shoes3'=>$shoes3]);
+        return view('PublicPage.SubPage.contents',['producer'=>$producer,'shoes'=>$shoes,'shoes2'=>$shoes2,'shoes3'=>$shoes3, 'request' => $request]);
     }
 
     public function getAllShoes(){
